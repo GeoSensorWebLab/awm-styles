14a9234527fdf2003bbdc63d476b8efde6b97ad4
James Badger <james@jamesbadger.ca>
2014-10-06 11:44:09 -0600

    Extract base shapefile layers to own file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for file in 3571 3572 3573 3574 3575 3576; do
-	/bin/cp osm_$file.xml /etc/mapnik-osm-carto-data/.
+for file in *.xml; do
+	/bin/cp $file /etc/mapnik-osm-carto-data/.
 done
