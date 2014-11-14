e59b0810198b6498f64b1c6e84a2d0feadf57e3c
James Badger <james@jamesbadger.ca>
2014-11-14 14:26:30 -0700

    Clean up redundant names in script

--- get-shapefiles.sh
+++ get-shapefiles.sh
@@ -14,73 +14,80 @@ mkdir -p data/land-polygons-split-3857
 mkdir -p data/ne_10m_geographic_lines
 
 # world_boundaries
-echo "dowloading world_boundaries..."
-curl -z "data/world_boundaries-spherical.tgz" -L -o "data/world_boundaries-spherical.tgz" "http://planet.openstreetmap.org/historical-shapefiles/world_boundaries-spherical.tgz"
-echo "expanding world_boundaries..."
-tar -xzf data/world_boundaries-spherical.tgz -C data/
+PACK="world_boundaries"
+echo "dowloading $PACK..."
+curl -z "data/$PACK-spherical.tgz" -L -o "data/$PACK-spherical.tgz" "http://planet.openstreetmap.org/historical-shapefiles/$PACK-spherical.tgz"
+echo "expanding $PACK..."
+tar -xzf data/$PACK-spherical.tgz -C data/
 
 # simplified-land-polygons-complete-3857
-echo "downloading simplified-land-polygons-complete-3857..."
-curl -z "data/simplified-land-polygons-complete-3857.zip" -L -o "data/simplified-land-polygons-complete-3857.zip" "http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip"
-echo "simplified-land-polygons-complete-3857..."
-unzip $UNZIP_OPTS data/simplified-land-polygons-complete-3857.zip \
-  simplified-land-polygons-complete-3857/simplified_land_polygons.shp \
-  simplified-land-polygons-complete-3857/simplified_land_polygons.shx \
-  simplified-land-polygons-complete-3857/simplified_land_polygons.prj \
-  simplified-land-polygons-complete-3857/simplified_land_polygons.dbf \
-  simplified-land-polygons-complete-3857/simplified_land_polygons.cpg \
+PACK="simplified-land-polygons-complete-3857"
+echo "downloading $PACK..."
+curl -z "data/$PACK.zip" -L -o "data/$PACK.zip" "http://data.openstreetmapdata.com/$PACK.zip"
+echo "$PACK..."
+unzip $UNZIP_OPTS data/$PACK.zip \
+  $PACK/simplified_land_polygons.shp \
+  $PACK/simplified_land_polygons.shx \
+  $PACK/simplified_land_polygons.prj \
+  $PACK/simplified_land_polygons.dbf \
+  $PACK/simplified_land_polygons.cpg \
   -d data/
 
 # ne_110m_admin_0_boundary_lines_land
-echo "dowloading ne_110m_admin_0_boundary_lines_land..."
-curl -z data/ne_110m_admin_0_boundary_lines_land.zip -L -o data/ne_110m_admin_0_boundary_lines_land.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_boundary_lines_land.zip
-echo "expanding ne_110m_admin_0_boundary_lines_land..."
-unzip $UNZIP_OPTS data/ne_110m_admin_0_boundary_lines_land.zip \
-  ne_110m_admin_0_boundary_lines_land.shp \
-  ne_110m_admin_0_boundary_lines_land.shx \
-  ne_110m_admin_0_boundary_lines_land.prj \
-  ne_110m_admin_0_boundary_lines_land.dbf \
-  -d data/ne_110m_admin_0_boundary_lines_land/
+PACK="ne_110m_admin_0_boundary_lines_land"
+echo "dowloading $PACK..."
+curl -z data/$PACK.zip -L -o data/$PACK.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/$PACK.zip
+echo "expanding $PACK..."
+unzip $UNZIP_OPTS data/$PACK.zip \
+  $PACK.shp \
+  $PACK.shx \
+  $PACK.prj \
+  $PACK.dbf \
+  -d data/$PACK/
 
 # ne_10m_populated_places
-echo "dowloading ne_10m_populated_places..."
-curl -z data/ne_10m_populated_places.zip -L -o data/ne_10m_populated_places.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places.zip
-echo "expanding ne_10m_populated_places..."
-unzip $UNZIP_OPTS data/ne_10m_populated_places.zip \
-  ne_10m_populated_places.shp \
-  ne_10m_populated_places.shx \
-  ne_10m_populated_places.prj \
-  ne_10m_populated_places.dbf \
-  ne_10m_populated_places.cpg \
-  -d data/ne_10m_populated_places/
+PACK="ne_10m_populated_places"
+echo "dowloading $PACK..."
+curl -z data/$PACK.zip -L -o data/$PACK.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/$PACK.zip
+echo "expanding $PACK..."
+unzip $UNZIP_OPTS data/$PACK.zip \
+  $PACK.shp \
+  $PACK.shx \
+  $PACK.prj \
+  $PACK.dbf \
+  $PACK.cpg \
+  -d data/$PACK/
 
 # land-polygons-split-3857
-echo "dowloading land-polygons-split-3857..."
-curl -z "data/land-polygons-split-3857.zip" -L -o "data/land-polygons-split-3857.zip" "http://data.openstreetmapdata.com/land-polygons-split-3857.zip"
-echo "expanding land-polygons-split-3857..."
-unzip $UNZIP_OPTS data/land-polygons-split-3857.zip \
-  land-polygons-split-3857/land_polygons.shp \
-  land-polygons-split-3857/land_polygons.shx \
-  land-polygons-split-3857/land_polygons.prj \
-  land-polygons-split-3857/land_polygons.dbf \
-  land-polygons-split-3857/land_polygons.cpg \
+PACK="land-polygons-split-3857"
+echo "dowloading $PACK..."
+curl -z "data/$PACK.zip" -L -o "data/$PACK.zip" "http://data.openstreetmapdata.com/$PACK.zip"
+echo "expanding $PACK..."
+unzip $UNZIP_OPTS data/$PACK.zip \
+  $PACK/land_polygons.shp \
+  $PACK/land_polygons.shx \
+  $PACK/land_polygons.prj \
+  $PACK/land_polygons.dbf \
+  $PACK/land_polygons.cpg \
   -d data/
 
 # ne_10m_geographic_lines
-echo "dowloading ne_10m_geographic_lines..."
-curl -z "data/ne_10m_geographic_lines.zip" -L -o "data/ne_10m_geographic_lines.zip" "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_geographic_lines.zip"
-echo "expanding ne_10m_geographic_lines..."
-unzip $UNZIP_OPTS data/ne_10m_geographic_lines.zip \
-  ne_10m_geographic_lines.shp \
-  ne_10m_geographic_lines.shx \
-  ne_10m_geographic_lines.prj \
-  ne_10m_geographic_lines.dbf \
-  -d data/ne_10m_geographic_lines/
+PACK="ne_10m_geographic_lines"
+echo "dowloading $PACK..."
+curl -z "data/$PACK.zip" -L -o "data/$PACK.zip" "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/$PACK.zip"
+echo "expanding $PACK..."
+unzip $UNZIP_OPTS data/$PACK.zip \
+  $PACK.shp \
+  $PACK.shx \
+  $PACK.prj \
+  $PACK.dbf \
+  -d data/$PACK/
 
 #process populated places
-echo "processing ne_10m_populated_places..."
-rm -f data/ne_10m_populated_places/ne_10m_populated_places_fixed.*
-ogr2ogr --config SHAPE_ENCODING UTF8 data/ne_10m_populated_places/ne_10m_populated_places_fixed.shp data/ne_10m_populated_places/ne_10m_populated_places.shp
+PACK="ne_10m_populated_places"
+echo "processing $PACK..."
+rm -f data/$PACK/${PACK}_fixed.*
+ogr2ogr --config SHAPE_ENCODING UTF8 data/$PACK/${PACK}_fixed.shp data/$PACK/$PACK.shp
 
 #index
 echo "indexing shapefiles"
