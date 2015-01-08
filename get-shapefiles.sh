72aa7d81960ba5af4705e10ea59e7bcadbedfbde
James Badger <james@jamesbadger.ca>
2015-01-07 18:34:02 -0700

    Download Natural Earth land cover
    10m physical vector. To replace simplified polygons 3857 from
    OSM, which has issues with self-intersecting polygons. These self-
    intersections cause GDAL to fail to crop it properly and may be
    the cause of exploded polygons at zoom levels 0-3.

--- get-shapefiles.sh
+++ get-shapefiles.sh
@@ -9,6 +9,7 @@ mkdir -p data/
 mkdir -p data/world_boundaries
 mkdir -p data/simplified-land-polygons-complete-3857
 mkdir -p data/ne_110m_admin_0_boundary_lines_land
+mkdir -p data/ne_10m_land
 mkdir -p data/land-polygons-split-3857
 mkdir -p data/ne_10m_geographic_lines
 mkdir -p data/ne_10m_graticules_15
@@ -33,6 +34,14 @@ unzip $UNZIP_OPTS data/$PACK.zip \
   $PACK/simplified_land_polygons.dbf \
   $PACK/simplified_land_polygons.cpg \
   -d data/
+
+# ne_10m_land
+PACK="ne_10m_land"
+echo "downloading $PACK..."
+curl -z "data/$PACK.zip" -L -o "data/$PACK.zip" "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/${PACK}.zip"
+echo "$PACK..."
+unzip $UNZIP_OPTS data/$PACK.zip \
+  -d data/$PACK
 
 # ne_110m_admin_0_boundary_lines_land
 PACK="ne_110m_admin_0_boundary_lines_land"
@@ -93,11 +102,23 @@ unzip $UNZIP_OPTS data/$PACK.zip \
 
 # Clip shapefiles to bounding box
 echo "clipping & segmenting shapefiles"
+SRS="EPSG:4326"
+
+PACK="ne_10m_land"
+rm -f data/$PACK/proc_*
+for shp in data/$PACK/*.shp; do
+  base=$(basename $shp .shp)
+  echo "Processing ${shp}…"
+  ogr2ogr -t_srs $SRS -clipdst -180 0 180 90 "data/$PACK/tmp_$base.shp" "$shp"
+  ogr2ogr -segmentize 0.5 "data/$PACK/proc_$base.shp" "data/$PACK/tmp_$base.shp"
+  rm -f data/$PACK/tmp_${base}*
+done
 
 rm -f data/ne_10m_bathymetry_all/proc_*
 rm -f data/ne_10m_bathymetry_all/tmp_*
 for shp in data/ne_10m_bathymetry_all/*.shp; do
   base=$(basename $shp .shp)
+  echo "Processing ${shp}…"
   ogr2ogr -clipsrc -180 45 180 90 "data/ne_10m_bathymetry_all/tmp_$base.shp" "$shp"
   ogr2ogr -segmentize 0.5 "data/ne_10m_bathymetry_all/proc_$base.shp" "data/ne_10m_bathymetry_all/tmp_$base.shp"
 done
@@ -109,6 +130,7 @@ echo "indexing shapefiles"
 shapeindex --shape_files \
 data/simplified-land-polygons-complete-3857/simplified_land_polygons.shp \
 data/land-polygons-split-3857/land_polygons.shp \
+data/ne_10m_land/proc_ne_10m_land.shp \
 data/ne_110m_admin_0_boundary_lines_land/ne_110m_admin_0_boundary_lines_land.shp \
 data/ne_10m_geographic_lines/ne_10m_geographic_lines.shp \
 data/ne_10m_geographic_lines/ne_10m_graticules_15.shp \
