674a0753f76b8061c7421b9f0a50f183d22b2a2e
James Badger <james@jamesbadger.ca>
2019-03-27 10:54:31 -0600

    Fix output format selection on compile tool
    Also switch to using child.spawn for better stdout/stderr — this
    prints to console better than the previous method.

--- compile.js
+++ compile.js
@@ -6,27 +6,35 @@ const child = require('child_process');
 
 let baseProject = "openstreetmap-carto/project.mml";
 let awmConfig   = "awmconfig.js";
-let format      = "XML";
-let outputFile  = "arcticwebmap.xml";
+let format      = "xml";
+let outputFile  = "arcticwebmap.png";
 
 let args = process.argv.slice(2, process.argv.length);
 
 if (args[0] !== undefined) {
 	switch(args[0].toLocaleUpperCase()) {
 		case 'MML':
-			format = "MML";
+			format = "mml";
 			outputFile = "arcticwebmap.mml";
 		break;
 		case 'YAML':
 		case 'YML':
-			format = "YAML";
+			format = "yml";
 			outputFile = "arcticwebmap.yml";
+		break;
+		case 'PNG':
+		case 'PNG8':
+			format = "png8";
+			outputFile = "arcticwebmap.png";
 		default:
 		// do nothing, use xml
 	}
 }
 
-let command = `${__dirname}/node_modules/.bin/kosmtik export --output ${outputFile} --localconfig ${awmConfig} --format mml ${baseProject}`;
-child.execSync(command, (error, stdout, stderr) => {
-	console.log(stdout);
-});
+child.spawn(`${__dirname}/node_modules/.bin/kosmtik`, 
+	['export', '--output', outputFile,
+	 '--localconfig', awmConfig,
+	 '--format', format,
+	 baseProject], {
+		stdio: 'inherit'
+	});
