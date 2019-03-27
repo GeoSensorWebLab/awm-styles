e9876c090340c106c24a41d2ca0c43b5fd28b90f
James Badger <james@jamesbadger.ca>
2019-03-26 18:24:47 -0600

    Improve overwrite config
    This seems to be the way to delete layers (such as Antarctic)

--- awmconfig.js
+++ awmconfig.js
@@ -1,9 +1,26 @@
+const pry = require('pryjs');
+
 exports.LocalConfig = function (localizer, project) {
-    localizer.where('center').then([29.9377, -3.4216, 9]);
-    localizer.where('Layer').if({'Datasource.type': 'postgis'}).then({
+    localizer.where('name').then("ArcticWebMap");
+    localizer.where('center').then([60, -90, 4]);
+
+    // Change database name, details, projection
+    localizer.where('Layer')
+    .if({'Datasource.type': 'postgis'})
+    .then({
         "Datasource.dbname": "osm",
         "Datasource.password": "",
         "Datasource.user": "",
-        "Datasource.host": ""
-    });
+        "Datasource.host": "",
+        "Datasource.extent": "-180,0,180,90",
+        "srs-name": "EPSG:4326",
+        "srs": "+proj=longlat +datum=WGS84 +no_defs",
+        "extent": [-180,0,180,90]
+	});
+
+	// remove antarctic layers
+	removedIDs = ['icesheet-outlines', 'icesheet-poly'];
+	project.mml.Layer = project.mml.Layer.filter((layer) => {
+		return !removedIDs.includes(layer.id);
+	});
 };
