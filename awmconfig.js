ab37736b846d879f68c6e6b95ded1ac456763983
James Badger <james@jamesbadger.ca>
2019-03-28 20:27:24 -0600

    Update conversion for shapefile changes
    Use NE data land polygons instead of OSM land polygons. NE data
    converts without geometry errors and OSM polygons have weird
    problems.
    
    Remove high zoom level land polygons. No replacement selected yet.
    
    Switch all shapefiles to EPSG:3573 projection. Projection is done
    ahead of time using GDAL.

--- awmconfig.js
+++ awmconfig.js
@@ -2,8 +2,15 @@ const pry = require('pryjs');
 
 exports.LocalConfig = function (localizer, project) {
     project.mml.name = "ArcticWebMap";
-    project.mml.center = [70, -90, 4];
-    project.mml.bounds = [-180, 0, 0, 90];
+    project.mml.center = [0, 0, 3];
+    project.mml.bounds = [
+        -20037508,
+        -20037508,
+        20037508,
+        20037508
+    ];
+    // Use EPSG:3573 PROJ4 string
+    project.mml.srs = "+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs";
 
     // Cache features in RAM. Uses a lot of RAM that would better be
     // used by Postgres.
@@ -27,12 +34,28 @@ exports.LocalConfig = function (localizer, project) {
         "extent": [-180,0,0,90]
 	});
 
-	// remove antarctic layers
-	removedIDs = ['icesheet-outlines', 'icesheet-poly'];
+	// remove antarctic layers, high-zoom coast
+	removedIDs = ['icesheet-outlines', 'icesheet-poly', 'coast-poly'];
 	project.mml.Layer = project.mml.Layer.filter((layer) => {
 		return !removedIDs.includes(layer.id);
 	});
 
-	// Use EPSG:3573 PROJ4 string
-	project.mml.srs = "+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs";
+    // Use natural earth 10m for land instead of OSM
+    localizer.where('Layer')
+    .if({'Datasource.file': 'data/simplified-land-polygons-complete-3857/simplified_land_polygons.shp'})
+    .then({
+        'Datasource.file': 'data/ne_10m_land/ne_10m_land.shp'
+    })
+
+    // Adjust layer file location, source projection
+    localizer.where('Layer')
+    .if({'Datasource.type': 'shape'})
+    .then((obj) => {
+        obj["srs-name"] = "EPSG:3573";
+        obj["srs"] = "+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs";
+        obj.extent = [-20037508, -20037508, 20037508, 20037508];
+        obj.Datasource.extent = [-20037508, -20037508, 20037508, 20037508];
+        obj.Datasource.file = obj.Datasource.file.replace(/^data/, 'data/awm');
+        return obj;
+    });	
 };
