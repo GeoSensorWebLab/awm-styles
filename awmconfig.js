25d9fb759b9ef2a9435eb35d7c18a90e4f0f6be7
James Badger <james@jamesbadger.ca>
2019-03-29 18:54:34 -0600

    Clean up adding new layers configuration

--- awmconfig.js
+++ awmconfig.js
@@ -73,10 +73,11 @@ exports.LocalConfig = function (localizer, project) {
         return obj;
     });
 
-    // Add shapefile layers after `necountries`
-    let necountriesIndex = project.mml.Layer.findIndex((item) => {
-        return item.id === "necountries";
-    });
+    // Add a layer to the Layer list after the layer with id `searchID`
+    function addAfter(searchID, layer) {
+        let index = project.mml.Layer.findIndex(l => (l.id === searchID));
+        project.mml.Layer.splice(index+1, 0, layer);
+    }
 
     // Sample layer format:
     // {
@@ -105,54 +106,49 @@ exports.LocalConfig = function (localizer, project) {
     //     }
     // }
 
-    if (necountriesIndex) {
-        let newLayers = [];
+    addAfter("necountries", {
+        id: "lakes-low",
+        geometry: "polygon",
+        "srs-name": "EPSG:3573",
+        srs: "+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs",
+        Datasource: {
+            file: "data/awm/ne_10m_lakes/ne_10m_lakes.shp",
+            type: "shape"
+        },
+        properties: {
+            maxzoom: 7
+        }
+    });
 
-        newLayers.push({
-            id: "lakes-low",
+    // Add bathymetry layers
+    let bathymetry = {
+        "bathymetry-10km": "ne_10m_bathymetry_A_10000",
+        "bathymetry-9km":  "ne_10m_bathymetry_B_9000",
+        "bathymetry-8km":  "ne_10m_bathymetry_C_8000",
+        "bathymetry-7km":  "ne_10m_bathymetry_D_7000",
+        "bathymetry-6km":  "ne_10m_bathymetry_E_6000",
+        "bathymetry-5km":  "ne_10m_bathymetry_F_5000",
+        "bathymetry-4km":  "ne_10m_bathymetry_G_4000",
+        "bathymetry-3km":  "ne_10m_bathymetry_H_3000",
+        "bathymetry-2km":  "ne_10m_bathymetry_I_2000",
+        "bathymetry-1km":  "ne_10m_bathymetry_J_1000",
+        "bathymetry-200m": "ne_10m_bathymetry_K_200",
+        "bathymetry-0m":   "ne_10m_bathymetry_L_0",
+    }
+
+    Object.keys(bathymetry).forEach((id) => {
+        let filename = bathymetry[id];
+        addAfter("necountries", {
+            id: id,
             geometry: "polygon",
             "srs-name": "EPSG:3573",
             srs: "+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs",
             Datasource: {
-                file: "data/awm/ne_10m_lakes/ne_10m_lakes.shp",
+                file: `data/awm/ne_10m_bathymetry_all/${filename}.shp`,
                 type: "shape"
-            },
-            properties: {
-                maxzoom: 7
             }
         });
-
-        let bathymetry = {
-            "bathymetry-0m":   "ne_10m_bathymetry_L_0",
-            "bathymetry-200m": "ne_10m_bathymetry_K_200",
-            "bathymetry-1km":  "ne_10m_bathymetry_J_1000",
-            "bathymetry-2km":  "ne_10m_bathymetry_I_2000",
-            "bathymetry-3km":  "ne_10m_bathymetry_H_3000",
-            "bathymetry-4km":  "ne_10m_bathymetry_G_4000",
-            "bathymetry-5km":  "ne_10m_bathymetry_F_5000",
-            "bathymetry-6km":  "ne_10m_bathymetry_E_6000",
-            "bathymetry-7km":  "ne_10m_bathymetry_D_7000",
-            "bathymetry-8km":  "ne_10m_bathymetry_C_8000",
-            "bathymetry-9km":  "ne_10m_bathymetry_B_9000",
-            "bathymetry-10km": "ne_10m_bathymetry_A_10000",
-        }
-
-        Object.keys(bathymetry).forEach((id) => {
-            let filename = bathymetry[id];
-            newLayers.push({
-                id: id,
-                geometry: "polygon",
-                "srs-name": "EPSG:3573",
-                srs: "+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs",
-                Datasource: {
-                    file: `data/awm/ne_10m_bathymetry_all/${filename}.shp`,
-                    type: "shape"
-                }
-            });
-        });
-
-        project.mml.Layer.splice(necountriesIndex + 1, 0, ...newLayers);
-    }
+    });
 
     // Add to stylesheets
     project.mml.Stylesheet.push({
